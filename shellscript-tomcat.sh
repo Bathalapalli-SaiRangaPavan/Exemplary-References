61506f976d9d0b1be50ce41441175952304400f7
Sai Ranga Pavan <121741348+Bathalapalli-SaiRangaPavan@users.noreply.github.com>
2023-03-05 16:18:11 +0530

    shellscript to install tomcat

--- shellscript-tomcat.sh
+++ shellscript-tomcat.sh
@@ -1,8 +1,3 @@
-# Shell Script to Install Tomcat 
-
-----------------------
-vi install-tomcat.sh 
------------------------
 #!/bin/bash
 amazon-linux-extras install java-openjdk11
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.6/bin/apache-tomcat-10.1.6.tar.gz
@@ -12,13 +7,4 @@ mv apache-tomcat-10.1.6 tomcat
 cd tomcat/bin/
 ./startup.sh
 
-### Give execute permissions for ypur script 
-chmod +x install-tomcat.sh
-### Run the shell script 
-./install-tomcat.sh
-
-#### Make sure you open port 8080 in security group
-##### To get Tomcat GUI 
-
-Browse - http://<giveyouripv4>:8080
 
