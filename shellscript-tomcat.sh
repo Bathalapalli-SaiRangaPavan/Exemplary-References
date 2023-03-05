8f679543c1a2623ff494c39d5010343b5127e715
Sai Ranga Pavan <121741348+Bathalapalli-SaiRangaPavan@users.noreply.github.com>
2023-03-05 16:11:07 +0530

    shellscript to install tomcat

--- shellscript-tomcat.sh
+++ shellscript-tomcat.sh
@@ -3,6 +3,7 @@
 ----------------------
 vi install-tomcat.sh 
 -----------------------
+#!/bin/bash
 amazon-linux-extras install java-openjdk11
 wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.6/bin/apache-tomcat-10.1.6.tar.gz
 tar -xvzf apache-tomcat-10.1.6.tar.gz
